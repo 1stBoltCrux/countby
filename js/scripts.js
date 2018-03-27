ce9bae78bd2c98d8ce76572841df4e52010844a3
Nick Kardish and Ted Leary <nick@>
2018-03-27 09:00:43 -0700

    add conditional statement to keep user from inputting bad numbers

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,12 @@ $(document).ready(function(){
   event.preventDefault();
     var number1 = parseInt($("input#countTo").val());
     var number2 = parseInt($("input#countBy").val());
-    for(var number3 = number2; number3 < number1; number3 += number2){
+    if (number1 < 0 || number1 < number2){
+      alert("Please enter a number to count to that is greater than zero and also a number to count by that is less than the number you're counting to!");
+    } else {
+      for(var number3 = number2; number3 <= number1; number3 += number2){
       $("ul").append("<li>" + number3 + "</li>");
+      }
     }
   });
 });
